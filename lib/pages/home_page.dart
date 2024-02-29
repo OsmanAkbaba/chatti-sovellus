a56aa0fd8ec7ff03660597599ee4fa9cc57ff11a
OsmanAkbaba <akbaba.98@hotmail.com>
2024-02-29 22:21:02 +0200

    lisätty viestinlähetys mahdollisuus, keskeneräinen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,72 @@
-
 import 'package:anna_sakkoa/components/my_drawer.dart';
+import 'package:anna_sakkoa/components/user_tile.dart';
+import 'package:anna_sakkoa/pages/chat_page.dart';
+import 'package:anna_sakkoa/services/auth/auth_service.dart';
+import 'package:anna_sakkoa/services/auth/chat/chat_services.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
- 
+  //chat & auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(" K o t i s i v u"),
-        
+        title: const Text(" K o t i s i v u"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context, snapshot) {
+          //error
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+          // loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading...");
+          }
+          //return list view
+          return ListView(
+            children: snapshot.data!
+                .map<Widget>(
+                    (userData) => _buildUserListItem(userData, context))
+                .toList(),
+          );
+        });
+  }
+
+  //build individual list tile for user
+  Widget _buildUserListItem(
+      Map<String, dynamic> userData, BuildContext context) {
+    // display all user except current user
+    if (userData["email"] != _authService.getCurrentUser()!.email) {
+      return UserTile(
+        text: userData["email"],
+        onTap: () {
+          //tapped on a user -< go to chat page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverEmail: userData["email"],
+                receiverID: userData["uid"],
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      return Container();
+    }
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,30 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  // insrance of auth
+  // insrance of auth & firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // sign in
+  // get current user stream
+  User? getCurrentUser () {
+    return _auth.currentUser;
+  }
+  // sign user in
   Future<UserCredential> signInWithEmailPassword(String email, passowrd) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: passowrd,
       );
+
+      //save user info if it does not already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email': email,
+        },
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      throw /*Exception*/ ("Väärä sähköposti tai salasana"); 
-      
+      throw Exception(e.code);
     }
   }
 
   //sign up
   Future<UserCredential> signUpWithEmailPassword(String email, passowrd) async {
     try {
+      //create user
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
         email: email,
         password: passowrd,
+      );
+
+      //save user data in a separate document
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email': email,
+        },
       );
 
       return userCredential;
